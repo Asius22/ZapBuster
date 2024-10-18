f13f72aaa17a730977d8b5ef998b96b1022c3ee3
Asius22 <ciro_2000@live.it>
2024-10-18 22:59:53 +0200

    addedd merge_wordlist function

--- utility.py
+++ utility.py
@@ -1,7 +1,6 @@
 import re
-import time
-
-_URL_REGEX = r'(http[s]?://[^\s]+)'
+import time, subprocess
+_URL_REGEX = r"(http[s]?://[^\s]+)"
 
 def extract_url_from_file(filename):
     res = set()
@@ -23,15 +22,35 @@ def extract_url_from_str(str):
 def waiting_print(str):
     
     for i in range(0, 3):
-        print("\r" + ' ' * 50 , end="")
+        print("\r" + " " * 50 , end="")
         print(f"\r{str} {". " * (i + 1)} ", end="")
 
         time.sleep(0.5)
 
 def progress_print(process, progress):
     
-    print("\r" + ' ' * 50 , end="")
+    print("\r" + " " * 50 , end="")
     print(f"\r{process}: {progress}% ", end="")
     if progress == "100":
         print("")
+        
+        
+
+def merge_wordlist(file1, file2, output):
+    """_summary
+    crea la seguente pipeline:
+        stampa file1 e file2
+        ordina le righe
+        cancella i duplicati
+        l"output finale stampalo in output
+    """
+    # Primo step: `cat file1 file2`
+    result1 = subprocess.run(["cat", file1, file2], stdout=subprocess.PIPE, text=True)
+
+    # Secondo step: `sort`, che prende l"output del primo step
+    result2 = subprocess.run(["sort"], input=result1.stdout, stdout=subprocess.PIPE, text=True)
+
+    # Terzo step: `uniq`, che prende l"output del secondo step e scrive nel file output.txt
+    with open(output, "w") as output_file:
+        subprocess.run(["uniq"], input=result2.stdout, stdout=output_file, text=True)
 
