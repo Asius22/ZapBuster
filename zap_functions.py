3537a3723770b30eeab4843298aba527486cdbe8
Asius22 <ciro_2000@live.it>
2024-10-18 22:58:30 +0200

    created ZAP class

--- zap_functions.py
+++ zap_functions.py
@@ -3,77 +3,79 @@ import subprocess
 import connection_manager as CM
 from utility import progress_print
 
-connection = CM.ConnectionManager()
-zap = connection.zap
+class ZAP:
+    def __init__(self, proxy):
+        self.connection = CM.ConnectionManager(proxy = proxy)
+        self.zap = self.connection.zap
 
-def zap_start_spider(url):
-    scanID = zap.spider.scan(url)
+    def start_spider(self, url):
+        scanID = self.zap.spider.scan(url)
 
-    while int(zap.spider.status(scanID)) < 100:
-        # Poll the status until it completes
-        progress_print(process="ZAP SPIDER", progress=zap.spider.status(scanID))
-        time.sleep(0.5)
+        while int(self.zap.spider.status(scanID)) < 100:
+            # Poll the status until it completes
+            progress_print(process="ZAP SPIDER", progress=self.zap.spider.status(scanID))
+            time.sleep(0.5)
 
-    print('\nSpider has completed!')
-    for res in zap.spider.results(scanID):
-        print(res)
+        print('\nSpider has completed!')
+        for res in self.zap.spider.results(scanID):
+            print(res)
 
 
-def zap_start_ajax_spider(url):
-    scanID = zap.ajaxSpider.scan(url)
+    def start_ajax_spider(self, url):
+        scanID = self.zap.ajaxSpider.scan(url)
 
-    timeout = time.time() + 60*2   # 2 minutes from now
-    # Loop until the ajax spider has finished or the timeout has exceeded
-    while zap.ajaxSpider.status == 'running':
-        if time.time() > timeout:
-            break
-        print(f"\r {' '*50}", end ="")
-        print(f'\rAjax Spider status: {zap.ajaxSpider.status}', end="" )
-        time.sleep(0.5)
+        timeout = time.time() + 60*2   # 2 minutes from now
+        # Loop until the ajax spider has finished or the timeout has exceeded
+        while self.zap.ajaxSpider.status == 'running':
+            if time.time() > timeout:
+                break
+            print(f"\r {' '*50}", end ="")
+            print(f'\rAjax Spider status: {self.zap.ajaxSpider.status}', end="" )
+            time.sleep(0.5)
 
-    print('\nAjax Spider completed')
-    zap.ajaxSpider.results()
-    
-def zap_insert_url_in_context(urls):
-    for url in urls:
-        zap.core.access_url(url, followredirects=None)
-
-def zap_get_nodes(url):
-    for node in zap.core.child_nodes(url):
-        print (node)
-# Inserisce la lista di url nel contesto di zap e procede con la scansione delle anomalie
-def zap_start_ascan(urls = []):
-
-    if len(urls) != 0:
-        zap_insert_url_in_context(urls)
-    
-    for site in zap.core.sites:
+        print('\nAjax Spider completed')
+        self.zap.ajaxSpider.results()
         
-        scanID = zap.ascan.scan(site)
+    def insert_url_in_context(self, urls):
+        for url in urls:
+            self.zap.core.access_url(url, followredirects=None)
 
-        try:
-            while int(zap.ascan.status(scanID)) < 100:
-                progress_print(process="[Active Scan]", progress=zap.ascan.status(scanID))
-                time.sleep(5)
+    def get_nodes(self, url):
+        for node in self.zap.core.child_nodes(self, url):
+            print (node)
+    # Inserisce la lista di url nel contesto di zap e procede con la scansione delle anomalie
+    def start_ascan(self, urls = []):
 
-            print('\nActive Scan completed')
-        except ValueError:
-            print("[ValueError] Si è verificato un errore con l'ActiveScan...")
-            print(f"scanID: {scanID}")
-            print("Chiusura")
-            connection.close()
+        if len(urls) != 0:
+            self.insert_url_in_context(urls)
+        
+        for site in self.zap.core.sites:
+            
+            scanID = self.zap.ascan.scan(site)
 
-        # Print vulnerabilities found by the scanning
+            try:
+                while int(self.zap.ascan.status(scanID)) < 100:
+                    progress_print(process="[Active Scan]", progress=self.zap.ascan.status(scanID))
+                    time.sleep(5)
 
-def zap_print_report():
-    REPORT_NAME = 'zap_report.html'
-    report = zap.core.htmlreport()
+                print('\nActive Scan completed')
+            except ValueError:
+                print("[ValueError] Si è verificato un errore con l'ActiveScan...")
+                print(f"scanID: {scanID}")
+                print("Chiusura")
+                self.connection.close()
 
-    with open( REPORT_NAME, 'w', encoding='utf-8') as report_file:
-        report_file.write(report)
-    print("Report creato!!")
-    subprocess.Popen(["firefox", REPORT_NAME])
+            # Print vulnerabilities found by the scanning
+
+    def print_report(self):
+        REPORT_NAME = 'report.html'
+        report = self.zap.core.htmlreport()
+
+        with open( REPORT_NAME, 'w', encoding='utf-8') as report_file:
+            report_file.write(report)
+        print("Report creato!!")
+        subprocess.Popen(["firefox", REPORT_NAME])
 
 
-def termZap():
-    connection.close()
+    def termZap(self):
+        self.connection.close()
